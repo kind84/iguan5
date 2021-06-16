87fa96409b20d61c11562664cf0e1ca5f0616072
Paolo Grisoli <paolo_g84@icloud.com>
2021-06-16 18:49:05 +0200

    restore main.zig

--- build.zig
+++ build.zig
@@ -9,4 +9,10 @@ pub fn build(b: *std.build.Builder) void {
     lib.addCSourceFile("./src/vendor/lz4.c", &.{});
     lib.linkLibC();
     lib.install();
+
+    var main_tests = b.addTest("src/main.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
 }

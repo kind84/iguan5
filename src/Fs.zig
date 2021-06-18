be89037785fb0e809bc8f52e49687c59b54bc302
Paolo Grisoli <paolo_g84@icloud.com>
2021-06-18 18:02:20 +0200

    fix memory leaks

--- src/Fs.zig
+++ src/Fs.zig
@@ -18,6 +18,7 @@ basePath: []const u8,
 
 pub fn init(allocator: *Allocator, basePath: []const u8) !Fs {
     var data_path = try path.join(allocator, &.{ basePath, "data.n5" });
+    errdefer allocator.free(data_path);
     var dir = try std.fs.openDirAbsolute(data_path, .{});
     defer dir.close();
 
@@ -25,6 +26,10 @@ pub fn init(allocator: *Allocator, basePath: []const u8) !Fs {
         .allocator = allocator,
         .basePath = data_path,
     };
+}
+
+pub fn deinit(self: *Fs) void {
+    self.allocator.free(self.basePath);
 }
 
 /// returns the datablock at the provided coordinates.
@@ -46,15 +51,33 @@ pub fn datasetAttributes(self: *Fs, datasetPath: []const u8) !DatasetAttributes 
 
 fn datablockPath(self: *Fs, datasetPath: []const u8, gridPosition: []i64) ![]u8 {
     var full_path = try path.join(self.allocator, &.{ self.basePath, datasetPath });
+    defer self.allocator.free(full_path);
     for (gridPosition) |gp| {
         var buf = try self.allocator.alloc(u8, 4096);
         errdefer self.allocator.free(buf);
         const gp_str = try fmt.bufPrint(buf, "{d}", .{gp});
-        full_path = try path.join(self.allocator, &.{ full_path, gp_str });
+        var temp_path = try path.join(self.allocator, &.{ full_path, gp_str });
+        defer self.allocator.free(temp_path);
+        full_path = try self.allocator.resize(full_path, temp_path.len);
+        std.mem.copy(u8, full_path, temp_path);
         self.allocator.free(buf);
     }
 
-    return full_path;
+    var final_path = try self.allocator.alloc(u8, full_path.len);
+    std.mem.copy(u8, final_path, full_path);
+    return final_path;
+}
+
+test "init" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = &gpa.allocator;
+
+    var path_buffer: [std.os.PATH_MAX]u8 = undefined;
+    var full_path = try std.fs.realpath("testdata/lynx_lz4", &path_buffer);
+    var fs = try Fs.init(allocator, full_path);
+    fs.deinit();
+
+    try std.testing.expect(!gpa.deinit());
 }
 
 test "lz4" {
@@ -64,17 +87,23 @@ test "lz4" {
     var path_buffer: [std.os.PATH_MAX]u8 = undefined;
     var full_path = try std.fs.realpath("testdata/lynx_lz4", &path_buffer);
     var fs = try Fs.init(allocator, full_path);
+    errdefer fs.deinit();
 
     var grid_position = [_]i64{ 0, 0, 0, 0, 0 };
 
     var attr = try fs.datasetAttributes("0/0");
-    defer attr.deinit();
-    std.debug.print("{}\n", .{attr});
+    errdefer attr.deinit();
     var d_block = try fs.getBlock("0/0", attr, &grid_position);
-    defer d_block.deinit();
+    errdefer d_block.deinit();
     var out = std.io.getStdOut();
     var buf = try allocator.alloc(u8, d_block.len);
+    errdefer allocator.free(buf);
     var n = try d_block.reader().read(buf);
-    defer allocator.free(buf);
     try out.writeAll(buf);
+
+    allocator.free(buf);
+    d_block.deinit();
+    attr.deinit();
+    fs.deinit();
+    try std.testing.expect(!gpa.deinit());
 }
